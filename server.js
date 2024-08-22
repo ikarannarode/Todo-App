bfe5d9b6bab15e796279401eca8cc27f112e2b3e
Karan Narode <karannarode999@gmail.com>
2024-08-22 23:59:55 +0530

    Basic changes

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ import { DatabaseConnection } from "./data/database.js"
 DatabaseConnection()
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+    console.log(`Server is running on http://localhost:${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 });
